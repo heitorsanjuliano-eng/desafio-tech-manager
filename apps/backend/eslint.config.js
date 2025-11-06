589c66f4a7d2464404cbe8b8dedeb4b451737e6b
heitorsanjuliano-eng <heitor.sanjuliano@gmail.com>
2025-11-06 13:56:18 -0300

    Entrega final do desafio técnico

--- apps/backend/eslint.config.js
+++ apps/backend/eslint.config.js
@@ -1,19 +1,15 @@
-import eslintPluginTs from "@typescript-eslint/eslint-plugin";
-import parser from "@typescript-eslint/parser";
+const eslintPluginTs = require("@typescript-eslint/eslint-plugin");
+const parser = require("@typescript-eslint/parser");
 
-export default [
+module.exports = [
   {
     files: ["src/**/*.ts"],
     languageOptions: {
       parser,
-      parserOptions: { project: "./tsconfig.json" },
     },
     plugins: {
       "@typescript-eslint": eslintPluginTs,
     },
-    rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/explicit-module-boundary-types": "off",
-    },
+    rules: {},
   },
 ];
